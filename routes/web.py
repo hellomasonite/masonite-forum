ea8603bade8b0162ab1602cc35703428af990719
Junior Gantin <nioperas06@gmail.com>
2018-07-08 03:44:20 +0100

    🚚 (fix) add auth middleware to logout

--- routes/web.py
+++ routes/web.py
@@ -1,10 +1,10 @@
 ''' Web Routes '''
-from masonite.helpers.routes import get, post
+from masonite.helpers.routes import get, post, group
 
 ROUTES = [
     get('/', 'HomeController@index').name('welcome'),
     get('/login', 'LoginController@show'),
-    get('/logout', 'LoginController@logout'),
+    get('/logout', 'LoginController@logout').middleware('auth'),
     post('/login', 'LoginController@store'),
     get('/register', 'RegisterController@show'),
     post('/register', 'RegisterController@store'),
