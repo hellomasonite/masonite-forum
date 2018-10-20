4b89e4b95b2aaf0c0f502a21dd6d54a679f8887f
Junior Gantin <nioperas06@gmail.com>
2018-10-20 09:42:02 +0100

    add pagination

--- app/http/controllers/HomeController.py
+++ app/http/controllers/HomeController.py
@@ -10,10 +10,13 @@ class HomeController(object):
         pass
     
     def index(self, Request):
-        questions = Question.all()
+        page = Request.input('page', 1)
+
+        questions = Question.paginate(10, int(page))
         categories = Category.all()
         
         search = Request.input('search')
+
         if search:
             questions = Question.where('title', 'like', '%{0}%'.format(search)).get()
 
