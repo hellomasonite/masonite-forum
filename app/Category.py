576d2e8255b49f03dce2ba55e0a29c9510918720
Junior Gantin <nioperas06@gmail.com>
2020-04-21 15:04:24 +0100

    clean repo

--- app/Category.py
+++
@@ -1,9 +0,0 @@
-''' A Category Database Model '''
-from config.database import Model
-import random
-
-class Category(Model):
-    __fillable__ = ['name']
-
-    def random_color(self):
-        return "#{:06x}".format(random.randint(0, 0xFFFFFF))
--- app/Vote.py
+++
@@ -1,7 +0,0 @@
-''' A Vote Database Model '''
-from config.database import Model
-
-class Vote(Model):
-    __table__ = 'votes'
-
-    __fillable__ = ['question_id', 'user_id', 'answer_id', 'value']
